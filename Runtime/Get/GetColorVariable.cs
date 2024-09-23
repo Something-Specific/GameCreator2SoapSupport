2d05a499f8bbc1fed8b57d54aa8ed770c5405d48
Jared Ross <jdross91@gmail.com>
2024-09-23 09:28:55 -0600

    Add ColorVariable getter

---
+++ Runtime/Get/GetColorVariable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using GameCreator.Runtime.Common;
+using Obvious.Soap;
+using UnityEngine;
+
+namespace SomethingSpecific.Ext.GameCreator.SOAP {
+   
+	[Title("ColorVariable")]
+    [Category("Soap/ColorVariable")]
+    [Image(typeof(IconString), ColorTheme.Type.Red)]
+    [Description("A SOAP ColorVariable")]
+    [Keywords("Color", "SOAP", "ColorVariable")]
+    [Serializable]
+    public class GetColorVariable : PropertyTypeGetColor {
+        
+        [SerializeField]
+        private Color defaultColor = Color.black;
+
+        [SerializeField]
+        private ColorVariable boolVariable;
+
+        public override Color Get(GameObject target) {
+            return GetColorVariableValue();
+        }
+
+        public override Color Get(Args args) {
+            return GetColorVariableValue();
+        }
+
+        public override string String => GetColorVariableValue().ToString();
+
+        private Color GetColorVariableValue() {
+            return boolVariable != null ? boolVariable.Value : defaultColor;
+        }
+    }
+}
