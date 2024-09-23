9c432885c3816375b3562b77fd9be4da8d67621d
Jared Ross <jdross91@gmail.com>
2024-09-23 09:29:06 -0600

    Add IntVariable getter

---
+++ Runtime/Get/GetIntVariable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using GameCreator.Runtime.Common;
+using Obvious.Soap;
+using UnityEngine;
+
+namespace SomethingSpecific.Ext.GameCreator.SOAP {
+   
+	[Title("IntVariable")]
+    [Category("Soap/IntVariable")]
+    [Image(typeof(IconString), ColorTheme.Type.Red)]
+    [Description("A SOAP IntVariable")]
+    [Keywords("Int", "SOAP", "IntVariable")]
+    [Serializable]
+    public class GetIntVariable : PropertyTypeGetDecimal {
+        
+        [SerializeField]
+        private int defaultValue = 0;
+
+        [SerializeField]
+        private IntVariable intVariable;
+
+        public override double Get(GameObject target) {
+            return GetIntVariableValue();
+        }
+
+        public override double Get(Args args) {
+            return GetIntVariableValue();
+        }
+
+        public override string String => GetIntVariableValue().ToString();
+
+        private int GetIntVariableValue() {
+            return intVariable != null ? intVariable.Value : defaultValue;
+        }
+    }
+}
