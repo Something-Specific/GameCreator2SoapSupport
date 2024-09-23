769ec215b4dcd7aa27e650742e3152549e1e4429
Jared Ross <jdross91@gmail.com>
2024-09-23 14:56:08 -0600

    Add QuaternionVariable getter support
    Uses `PropertyTypeGetRotation` to allow using a SOAP QuaternionVariable as the value of an action/condition that uses the `PropertyTypeGetRotation` type.

---
+++ Runtime/Property/Get/Variable/GetQuaternionVariable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using GameCreator.Runtime.Common;
+using Obvious.Soap;
+using UnityEngine;
+
+namespace SomethingSpecific.Ext.GameCreator.SOAP {
+    [Title("Soap Variable")]
+    [Category("Soap/Quaternion Variable")]
+    [Image(typeof(IconRotation), ColorTheme.Type.Green)]
+    [Description("A SOAP QuaternionVariable")]
+    [Serializable]
+    public class GetQuaternionVariable : PropertyTypeGetRotation {
+        [SerializeField]
+        private QuaternionVariable quaternionVariable;
+
+        public override Quaternion Get(Args args) {
+            return quaternionVariable == null ? Quaternion.identity : quaternionVariable.Value;
+        }
+        
+        public static PropertyGetRotation Create() {
+            return new PropertyGetRotation(
+                new GetQuaternionVariable()
+            );
+        }
+
+        public override string String => quaternionVariable == null ? "None" : quaternionVariable.Value.ToString();
+    }
+}
